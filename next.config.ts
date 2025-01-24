e419c2f00b206ee30ea4fa86e106cbd8bc6e1d63
ebukacodes21 <georgeokafo1@gmail.com>
2025-01-24 17:31:43 +0100

    modify dist

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+  output: "export",
+  distDir: "dist",
 };
 
 export default nextConfig;
